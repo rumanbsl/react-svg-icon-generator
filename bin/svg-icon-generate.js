09651b65ab1a74078c05ec67f64ccd33fa4ecc6a
Ondrej Bartas <ondrej@bartas.cz>
2017-02-12 21:39:41 +0100

    Fix typo and lint

--- bin/svg-icon-generate.js
+++ bin/svg-icon-generate.js
@@ -3,16 +3,16 @@ const path = require('path');
 const configureGenerator = require('../lib/configureGenerator');
 
 const argv = require('yargs')
-    .default('reactPureRender', true)
-    .default('radium', true)
-    .default('native', false)
-    .default('keepFillColor', false)
-    .default('template', path.join(__dirname, '..', 'template', 'icon.nunjucks'))
-    .usage('Usage: \n$0 --svgDir [./assets/svgs] --destination [./src/components/Icon.jsx]')
-    .demandOption(['svgDir','destination'])
-    .argv;
+  .default('reactPureRender', true)
+  .default('radium', true)
+  .default('native', false)
+  .default('keepFillColor', false)
+  .default('template', path.join(__dirname, '..', 'template', 'icon.nunjucks'))
+  .usage('Usage: \n$0 --svgDir [./assets/svgs] --destination [./src/components/Icon.jsx]')
+  .demandOption(['svgDir','destination'])
+  .argv;
 
-console.log('Running SVG Icon generator with those options:');
+console.log('Running SVG Icon generator with the following options:');
 console.log(JSON.stringify(argv, null, 2));
 
 configureGenerator(argv)();
