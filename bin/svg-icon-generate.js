f71a359f5c63cd6ea654f3fb499d8a69d303b441
Ruman <rumanbsl@gmail.com>
2020-04-27 21:18:18 +0300

    parse "true","false" to boolean fix

--- bin/svg-icon-generate.js
+++ bin/svg-icon-generate.js
@@ -3,16 +3,20 @@ const path = require('path');
 const configureGenerator = require('../lib/configureGenerator');
 
 const argv = require('yargs')
-  .default('reactPureRender', true)
-  .default('radium', true)
-  .default('native', false)
-  .default('keepFillColor', false)
-  .default('template', path.join(__dirname, '..', 'template', 'icon.nunjucks'))
+  .options({
+    reactPureRender: { type: "boolean", default: true },
+    radium: { type: "boolean", default: true },
+    native: { type: "boolean", default: false },
+    keepFillColor: { type: "boolean", default: false },
+    template: { type: "string", default: path.join(__dirname, '..', 'template', 'icon.nunjucks') },
+    verbose: { type: "boolean", default: false },
+  })
   .usage('Usage: \n$0 --svgDir [./assets/svgs] --destination [./src/components/Icon.jsx]')
-  .demandOption(['svgDir','destination'])
-  .argv;
+  .demandOption(['svgDir', 'destination'])
+  .argv
 
-console.log('Running SVG Icon generator with the following options:');
-console.log(JSON.stringify(argv, null, 2));
+if (argv.verbose) {
+  console.table(argv);
+}
 
 configureGenerator(argv)();
