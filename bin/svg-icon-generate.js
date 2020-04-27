03f946ffc2aa054f2078a6f27c1e1046dbdd31c0
Ruman <rumanbsl@gmail.com>
2020-04-27 22:14:46 +0300

    tsx WIP

--- bin/svg-icon-generate.js
+++ bin/svg-icon-generate.js
@@ -4,8 +4,9 @@ const configureGenerator = require('../lib/configureGenerator');
 
 const argv = require('yargs')
   .options({
-    reactPureRender: { type: "boolean", default: true },
-    radium: { type: "boolean", default: true },
+    tsx: { type: "boolean", default: true },
+    reactPureRender: { type: "boolean", default: false },
+    radium: { type: "boolean", default: false },
     native: { type: "boolean", default: false },
     keepFillColor: { type: "boolean", default: false },
     template: { type: "string", default: path.join(__dirname, '..', 'template', 'icon.nunjucks') },
